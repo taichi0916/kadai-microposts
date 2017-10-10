65bbf1604edf1781b780fc46a90dc52842c6690f
taichi imai <imai0916taiti@gmail.com>
2017-10-10 06:36:34 +0000

    post

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
     end
   end  
 
-  def destroy
+  def destory
     session[:user_id] = nil
     flash[:success] = 'ログアウトしました。'
     redirect_to root_url
