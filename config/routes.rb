a6cb8586b6dbefbccafbd1834ba29149633d3b04
taichi imai <imai0916taiti@gmail.com>
2017-10-14 07:19:21 +0000

    follow, unfollow

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,15 @@ Rails.application.routes.draw do
  delete 'logout', to: 'sessions#destory'
  
  get 'signup', to: 'users#new'
- resources :users, only: [:index, :show, :new, :create]
-
+ resources :users, only: [:index, :show, :new, :create] do
+   member do
+     get :followings
+     get :followers
+   end
+   collection do 
+    ger :search
+ end
+ 
  resources :microposts, only: [:create, :destory]
+ resources :relationships,only: [:create, :destory]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171010044028) do
+ActiveRecord::Schema.define(version: 20171014061811) do
 
   create_table "microposts", force: :cascade do |t|
     t.string   "content"
@@ -18,6 +18,16 @@ ActiveRecord::Schema.define(version: 20171010044028) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "follow_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["follow_id"], name: "index_relationships_on_follow_id"
+    t.index ["user_id", "follow_id"], name: "index_relationships_on_user_id_and_follow_id", unique: true
+    t.index ["user_id"], name: "index_relationships_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
